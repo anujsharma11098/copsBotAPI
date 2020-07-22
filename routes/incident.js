79f46582939ca81fb55ad73bafe7b155b2b14bc9
Ekansh Jain <ejekanshjain@gmail.com>
2020-07-22 20:11:48 +0530

    now api takes evidence url instead of image

--- routes/incident.js
+++ routes/incident.js
@@ -2,7 +2,6 @@ const express = require('express')
 
 const Incident = require('../models/incident')
 const authToken = require('../middlewares/authToken')
-const { bucket, uploader } = require('../util/imageUpload')
 
 const router = express.Router()
 
@@ -11,22 +10,8 @@ router.get('/', async (req, res) => {
     res.json({ status: 200, incidents })
 })
 
-router.post('/', uploader.single('evidence'), authToken, async (req, res) => {
-    console.log(req.body);
-    console.log(req.file);
-    if (!req.file)
-        return res.status(400).json({
-            status: 400,
-            success: false,
-            message: 'No File Provided!'
-        })
-    if (req.fileValidationError)
-        return res.status(400).json({
-            status: 400,
-            success: false,
-            message: req.fileValidationError
-        })
-    const { incidentDesc, iLatitude, iLongitude } = req.body
+router.post('/', authToken, async (req, res) => {
+    const { incidentDesc, iLatitude, iLongitude, evidence } = req.body
     if (!incidentDesc)
         return res.status(400).json({
             status: 400,
@@ -45,42 +30,26 @@ router.post('/', uploader.single('evidence'), authToken, async (req, res) => {
             success: false,
             message: 'iLongitude is required!'
         })
+    if (!evidence)
+        return res.status(400).json({
+            status: 400,
+            success: false,
+            message: 'evidence is required!'
+        })
     try {
-        const blob = bucket.file(req.file.originalname)
-
-        const blobWriter = blob.createWriteStream({
-            metadata: {
-                contentType: req.file.mimetype
-            }
+        const incident = await Incident.create({
+            userId: req.user._id,
+            incidentDesc,
+            iLatitude,
+            iLongitude,
+            evidence
         })
-
-        blobWriter.on('error', err => {
-            console.log('\x1b[31m%s\x1b[0m', err)
-            res.status(500).json({
-                status: 500,
-                success: false,
-                message: 'Something Went Wrong!'
-            })
+        res.status(201).json({
+            status: 201,
+            success: true,
+            message: 'Added Successfully!',
+            incident
         })
-
-        blobWriter.on('finish', async () => {
-            const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURI(blob.name)}?alt=media`
-            const incident = await Incident.create({
-                userId: req.user._id,
-                incidentDesc,
-                iLatitude,
-                iLongitude,
-                evidence: publicUrl
-            })
-            res.status(201).json({
-                status: 201,
-                success: true,
-                message: 'Added Successfully!',
-                incident
-            })
-        })
-
-        blobWriter.end(req.file.buffer)
     } catch (err) {
         console.log(err)
         res.status(500).json({
