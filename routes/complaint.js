1436171cd02ec20703d05330e594be7cc4032050
Anuj Sharma <48546869+anujsharma11098@users.noreply.github.com>
2020-08-01 18:02:48 +0530

    up

--- routes/complaint.js
+++ routes/complaint.js
@@ -55,7 +55,7 @@ router.get('/', authAdmin, async (req, res) => {
                     }
                 }, {
                     $sort: {
-                        createdAt: -1
+                        createdAt: 1
                     }
                 }
             ])
